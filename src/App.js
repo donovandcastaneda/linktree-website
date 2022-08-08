3f5e0b2c2db0933353e8f2d3528a53f66897fc4e
Donovan Castaneda <donovandcastaneda@gmail.com>
2022-08-07 21:36:08 -0500

    fixed the primeflex css
    removed -p tag and added flex.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
 
 import { Avatar } from "primereact/avatar";
 import { Button } from "primereact/button";
+
 function App() {
+  // const colors = {
+  //   youtube: "red",
+  //   linkedin: "blue",
+  //   github: "green",
+  // };
+
   const data = {
     name: "Donovan Castaneda",
     image: "http://github.com/donovandcastaneda.png",
@@ -23,9 +30,30 @@ function App() {
       },
     ],
   };
+
   return (
-    <div>
-      <Avatar image={data.image} size="xlarge" shape="circle"></Avatar>
+    <div className="font-Snippet">
+      <div className="m-6">
+        <div className="flex align-items-center justify-content-center">
+          <Avatar image={data.image} size="xlarge" shape="circle"></Avatar>
+          <h1 className="m-2">{data.name}</h1>
+        </div>
+
+        <div className="flex align-items-center justify-content-center p-2">
+          <p>{data.bio}</p>
+        </div>
+
+        <div className="flex align-items-center justify-content-center m-2 ">
+          <div className="flex flex-column ">
+            {data.links.map((link) => (
+              <Button className="m-2">
+                <i className={`pi pi-${link.icon} px-3 `}></i>
+                {link.name}
+              </Button>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
