db1fec539cadb8423c7c6c63b4d87a77edc46bfa
Donovan Castaneda <donovandcastaneda@gmail.com>
2022-08-12 12:07:39 -0500

    changed icons and links

--- src/App.js
+++ src/App.js
@@ -19,39 +19,54 @@ function App() {
     bio: "Software Engineering Student at University of Kansas City, Missouri",
     links: [
       {
-        name: "my open source Github",
+        name: "my open source github",
         url: "http://github.com/donovandcastaneda",
         icon: "github",
+      },
+      {
+        name: "follow me on linkedin",
+        url: "http://linkedin.com/donovandcastaneda",
+        icon: "linkedin",
+      },
+      {
+        name: "send me a email",
+        url: "mailto:donovandcastaneda@gmail.com",
+        icon: "envelope",
       },
       {
         name: "follow me on twitter",
         url: "http://twitter.com/donovandcastaneda",
         icon: "twitter",
       },
+      {
+        name: "follow me on instagram",
+        url: "http://instagram.com/donovandcastaneda",
+        icon: "instagram",
+      },
     ],
   };
 
   return (
-    <div className="font-Snippet">
-      <div className="m-6">
-        <div className="flex align-items-center justify-content-center">
-          <Avatar image={data.image} size="xlarge" shape="circle"></Avatar>
-          <h1 className="m-2">{data.name}</h1>
-        </div>
+    <div className="m-6">
+      <div className="flex align-items-center justify-content-center">
+        <Avatar image={data.image} size="xlarge" shape="circle"></Avatar>
+        <h1 className="m-2">{data.name}</h1>
+      </div>
 
-        <div className="flex align-items-center justify-content-center p-2">
-          <p>{data.bio}</p>
-        </div>
+      <div className="flex align-items-center justify-content-center p-2">
+        <p>{data.bio}</p>
+      </div>
 
-        <div className="flex align-items-center justify-content-center m-2 ">
-          <div className="flex flex-column ">
-            {data.links.map((link) => (
-              <Button className="m-2">
-                <i className={`pi pi-${link.icon} px-3 `}></i>
-                {link.name}
-              </Button>
-            ))}
-          </div>
+      <div className="flex align-items-center justify-content-center  m-2 ">
+        <div className="flex flex-column ">
+          {data.links.map((link) => (
+            <Button className="m-2 shadow-5">
+              <i className={`pi pi-${link.icon} px-3 `}></i>
+              {link.name}
+            </Button>
+          ))}
+
+          <div> </div>
         </div>
       </div>
     </div>
